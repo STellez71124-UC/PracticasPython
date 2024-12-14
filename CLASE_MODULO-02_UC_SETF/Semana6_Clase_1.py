e9db721d579b9a88f9c11f987b6f29b2db4b069a
BCUS71124 <simoneebelmondo@gmail.com>
2024-12-14 11:52:12 -0600

    Fundamentos de Python C28

--- CLASE_MODULO-02_UC_SETF/Semana6_Clase_1.py
+++ CLASE_MODULO-02_UC_SETF/Semana6_Clase_1.py
@@ -4,21 +4,3 @@ while x>0:
 
 for i in range (1,101):
     print ('No debo platicar en clase.')
-
-palabracorrecta = 'Dinosaurio'
-numerointentos = 0
-maxintentos = 3
-while numerointentos < maxintentos:
-    print ('Hola, se me viene a la mente un animal famoso en el cine.')
-    adivina = input ('Adivina la palabra en que estoy pensando: ').title
-    numerointentos += 1
-    if adivina == palabracorrecta:
-        print ('Es correcto.')
-    elif numerointentos == 1:
-        print ('Una pista. Seguramente lo tuviste como juguete de niño.')
-    elif numerointentos == 2:
-        print ('Otra pista. Ya no existe en la actualidad')
-    elif numerointentos == 3:
-        print ('No le atinaste. Quiza en la siguiente ocasión lo adivines correctamente.')
-    else:
-        print ('Lo siento, no entendí tu respuesta.')
