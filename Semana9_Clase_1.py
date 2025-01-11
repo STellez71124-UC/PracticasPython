798516fee55da48893149888d1e5b288a67a22d5
BCUS71124 <simoneebelmondo@gmail.com>
2025-01-11 11:02:59 -0600

    Fundamentos de Python C28

---
+++ Semana9_Clase_1.py
@@ -0,0 +1,28 @@
+numeros = [10, 20, 30, 40]
+print (numeros[2])
+
+def suma_operacion ():
+    alpha = int(input('Valor númerico: '))
+    beta = int(input('Valor númerico: '))
+    resultado = alpha + beta
+    print (resultado)
+
+suma_operacion()
+
+def saludo_mundo():
+    print ('¡Hola Mundo!')
+
+saludo_mundo()
+
+def cuadrado_numero():
+    ingreso = int(input ('Número: '))
+    valorcuadrado = ingreso**2
+    print (valorcuadrado)
+
+cuadrado_numero()
+
+def saludo_persona():
+    nombre = input('Ingresa tu nombre: ')
+    print (f'¡Hola {nombre}!')
+
+saludo_persona()
